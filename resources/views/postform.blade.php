dca25cdaf4773e913f7055b65f7abdc41f2a26b2
oriental <orientalinformation@gmail.com>
2017-08-13 16:21:34 +0700

    Laravel blade template loop and if else

--- resources/views/postform.blade.php
+++ resources/views/postform.blade.php
@@ -1,5 +1,37 @@
 <form action="{{ route('postform') }}" method="post">
+	{{ csrf_field() }}
 	<input type="text" name="name">
 	<input type="submit">
-	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+	<!-- <input type="hidden" name="_token" value="{{ csrf_token() }}"> -->
 </form>
+
+{{  $a='<b>toidicode.com</b>'   }} <br> 
+
+{!!  $a='<b>toidicode.com</b>'  !!}  <br>
+
+<!--print data default use OR-->
+{{  $b or 5 }} <br>
+
+<!-- for-->
+@for($i = 0; $i < 10; $i++)
+	print data {{ $i }}
+@endfor
+
+@foreach ($users as $user)
+    <p>This is user {{ $user->id }}</p>
+@endforeach
+
+@forelse($users as $user)
+    <li>{{ $user->name }}</li>
+@empty
+    <p>No users</p>
+@endforelse
+
+
+@if (count($records) === 1)
+    I have one record!
+@elseif (count($records) > 1)
+    I have multiple records!
+@else
+    I don't have any records!
+@endif
--- resources/views/uploadfile.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="/public/postfile" method="post" enctype="multipart/form-data">
-	{{csrf_field()}}
-	<input type="file" name="myfile">
-	<input type="submit">
-	<!-- <input type="hidden" name="_token" value="{{ csrf_token() }}">﻿ -->
-</form>
